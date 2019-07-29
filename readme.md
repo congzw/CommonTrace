8ffb4a3a38bc7299dea6bdd07fcafefb5dbf4aa8
congzw <2981810300@qq.com>
2019-07-29 15:32:23 +0800

    init projects

---
+++ readme.md
@@ -0,0 +1,14 @@
+# CommonTrace
+
+## system components
+
+- CommonTrace
+- CommonTrace.Api
+- CommonTrace.Jaeger
+- CommonTrace.Shared
+
+## todo list
+
+- add trace client model and interface
+- add common utils
+- [x] init projects
---
+++ src/CommonTrace.Api/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommonTrace.Api.Controllers
+{
+    //for test only
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [Route("GetDate")]
+        [HttpGet]
+        public Task<DateTime> GetDate()
+        {
+            return Task.FromResult(DateTime.Now);
+        }
+    }
+}
