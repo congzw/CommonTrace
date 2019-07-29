d1b39aec9dec8f4f2f9976ee05e1a4d0ba344ef0
congzw <2981810300@qq.com>
2019-07-29 15:51:25 +0800

    add SimpleLog

---
+++ src/CommonTrace.Shared/Common/DateHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CommonTrace.Common
+{
+    public class DateHelper
+    {
+        public string GetNowAsFormat(string format = "yyyyMMdd-HH:mm:ss")
+        {
+            var now = GetDateNow();
+            return now.ToString(format);
+        }
+
+        public Func<DateTime> GetDateNow = () => DateTime.Now;
+        public static DateHelper Instance = new DateHelper();
+    }
+}
---
+++ src/CommonTrace.Shared/Common/SimpleLog.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace CommonTrace.Common
+{
+    //类库内部使用的简单日志，默认日志输出文件不启用
+    public interface ISimpleLog
+    {
+        bool LogFileEnabled { get; set; }
+        void Log(object message);
+        void LogEx(object ex);
+    }
+
+    internal class SimpleLog : ISimpleLog
+    {
+        public string Category { get; set; }
+
+        public bool LogFileEnabled { get; set; }
+
+        public void Log(object message)
+        {
+            Trace.WriteLine(string.Format("[{0}][{1}] {2}", "SimpleLog", Category, message));
+            if (LogFileEnabled)
+            {
+                if (message != null)
+                {
+                    LogFile(message.ToString(), "log");
+                }
+            }
+        }
+
+        public void LogEx(object ex)
+        {
+            if (ex == null)
+            {
+                return;
+            }
+
+            var content = string.Empty;
+            if (ex is string theString)
+            {
+                content = theString;
+            }
+            else if (ex is Exception theEx)
+            {
+                content = theEx.Message;
+            }
+            Trace.WriteLine(string.Format("[{0}][{1}][Ex] {2}", "SimpleLog", Category, content));
+
+            if (LogFileEnabled)
+            {
+                if (!string.IsNullOrWhiteSpace(content))
+                {
+                    LogFile(content, "logEx");
+                }
+            }
+        }
+        
+        private void LogFile(string message, string filePrefix)
+        {
+            var now = DateHelper.Instance.GetDateNow();
+            message = now.ToString("yyyyMMdd-HH:mm:ss ") + message;
+            var fileName = string.Format("{0}_{1:yyyyMMdd}.log", filePrefix, now);
+            var logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+            //async save file, ignore ex
+            AsyncFile.Instance.AppendAllText(logFilePath, message, true);
+        }
+
+        public static ISimpleLog Instance = new SimpleLog() { Category = "Default" };
+    }
+
+    public class SimpleLogFactory
+    {
+        public SimpleLogFactory()
+        {
+            CreateLog = c => new SimpleLog() { Category = c };
+        }
+
+        public Func<string, ISimpleLog> CreateLog { get; set; }
+
+        public static SimpleLogFactory Instance = new SimpleLogFactory();
+    }
+
+    public static class SimpleLogFactoryExtensions
+    {
+        public static ISimpleLog CreateLogFor(this SimpleLogFactory factory, Type type)
+        {
+            return factory.CreateLog(type.Name);
+        }
+
+        public static ISimpleLog CreateLogFor<T>(this SimpleLogFactory factory)
+        {
+            return factory.CreateLogFor(typeof(T));
+        }
+
+        public static ISimpleLog CreateLogFor(this SimpleLogFactory factory, object instance)
+        {
+            if (instance == null)
+            {
+                return SimpleLog.Instance;
+            }
+            if (instance is Type type)
+            {
+                return factory.CreateLogFor(type);
+            }
+            return factory.CreateLogFor(instance.GetType());
+        }
+    }
+}
