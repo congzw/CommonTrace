b2053574d224b5bf48e828ba00c4d0435ba0281c
congzw <2981810300@qq.com>
2019-07-30 10:52:32 +0800

    add Demo.ConsoleApp

---
+++ src/Demo.ConsoleApp/Demos/DemoRunner.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Demo.ConsoleApp.Demos
+{
+    public class DemoRunner
+    {
+        public static DemoRunner Instance = new DemoRunner();
+
+        public Task TaskExBad()
+        {
+            var task1 = Task.Run(() => throw new Exception("task1 faulted."));
+            task1.ContinueWith(t => Console.WriteLine("ex!!!"), TaskContinuationOptions.OnlyOnFaulted);
+            task1.ContinueWith(t => { /* on success */ }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            return task1;
+        }
+
+        public Task TaskEx(bool fail)
+        {
+            var task = Task.Run(() =>
+            {
+                if (fail)
+                {
+                    throw new Exception("task run faulted.");
+                }
+                else
+                {
+                    Console.WriteLine("task run success");
+                }
+            });
+            var failTask = task.ContinueWith(LogIfErrors, TaskContinuationOptions.OnlyOnFaulted);
+            var resultTask = Task.WhenAny(failTask, task);
+            return resultTask;
+        }
+
+        public async Task AsyncTaskEx()
+        {
+            try
+            {
+                //var task = Task.Factory.StartNew(() => throw new Exception("task1 faulted.")).ConfigureAwait(false);
+                var task = Task.Run(() => throw new Exception("task1 faulted.")).ConfigureAwait(false);
+                await task;
+            }
+            catch (Exception e)
+            {
+                // Perform cleanup here.
+                Console.WriteLine("Ex: " + e.Message);
+            }
+        }
+        
+        private static void LogIfErrors(Task source)
+        {
+            if (source.Exception == null)
+            {
+                return;
+            }
+            source.Exception.Handle(ex =>
+            {
+                Console.WriteLine("#unhandled task error: {0}", ex.Message);
+                return true;
+            });
+        }
+    }
+}
---
+++ src/Demo.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Demo.ConsoleApp.Demos;
+
+namespace Demo.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DemoRunner.Instance.TaskExBad().Wait();
+            DemoRunner.Instance.TaskEx(true).Wait();
+            DemoRunner.Instance.TaskEx(false).Wait();
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Delay(200).Wait();
+                Console.Write(".");
+            }
+            Console.WriteLine("main method complete.");
+            Console.Read();
+        }
+
+    }
+}
